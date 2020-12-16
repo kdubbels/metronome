316b236f0cf34749f00a23c65cce148f81250db2
Kristofer Dubbels <dubbk381@alumni.newschool.edu>
2020-12-16 00:19:37 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+/** @jsxImportSource @emotion/react */
+import { useState, useEffect } from 'react';
+import { Howl } from 'howler';
+import { jsx, css, keyframes } from '@emotion/react'
+
 import './App.css';
 
+// Example metronome
+// 
+// https://www.youtube.com/watch?v=ZK1omlZPCnY
+
+const line = keyframes`
+  0% {
+    background: #FCD7D6;
+  }
+  30% {
+    background: #F36E6B;
+  }
+`
+
+// here the duration shoudl be set dynamically as the speed for the metronome
+//
+// user inputs number. Divide that number (bpm) by 60, and voila!
+
+const forward = css`
+  animation: ${line} .5s linear infinite;
+    `
+
+function Holder({i, setCurrentNote}) {
+  return(
+    <span className='holder' css={forward} data-note={i} onAnimationIteration={() => setCurrentNote(i)}>
+      <div className='note'>
+        <div className='ripple'></div>
+      </div>
+  </span>
+  )
+}
+
+const notes = new Array(2).fill(null).map((x,i) => x = i);
+
+const snare = new Howl({
+  src: './sounds/snare.wav'
+});
+
 function App() {
+  const [note, setNote] = useState(null);
+  useEffect(() => {
+    if (note === 0) {
+      snare.play();
+    }
+    // if (note === 1) {
+    //   snare.play();
+    // }
+    console.warn(note);
+  }, [note])
+
+  const setCurrentNote = (i) => {
+    setNote(i)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="forward">
+        {notes.map((_, i) => {
+          return <Holder i={i} setCurrentNote={setCurrentNote}/>;
+        })}
+      </div>
+      <div>
+      </div>
     </div>
   );
 }
